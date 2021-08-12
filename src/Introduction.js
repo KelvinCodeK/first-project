7308a21bf57eb4e9b855b2434334ebce73a4c7de
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-08-13 01:01:33 +0200

    styling

--- src/Introduction.js
+++ src/Introduction.js
@@ -4,15 +4,15 @@ import React from 'react';
 // Maak de intro niet noodzakelijk om doorheen te gaan.
 
 export default class Introduction extends React.Component {
-  
+
   render() {
   return (
       <header>
         <h1>Weather To Sell</h1>
         <p>Bepaal jouw optimale verkoopmomenten</p>
         <div className="buttons">
-        <button>hoe werkt het?</button>
-        <button>aan de slag!</button>
+        <button className="buttonEen" style={this.props.styling} onMouseOver={this.props.handler} onMouseOut={this.props.handlerOut}>hoe werkt het?</button>
+        <button style={this.props.styling2} onMouseOver={this.props.handler} onMouseOut={this.props.handlerOut}>aan de slag!</button>
         </div>
         <section className="intro">
         
--- src/index.js
+++ src/index.js
@@ -4,9 +4,51 @@ import './index.css';
 import './norm.css'
 import Introduction from './Introduction';
 
+// houdt de standaard structuur aan. Als dingen gebeuren zet je pas je de state aan en en stuur je de nieuwe state door met props
+// conditionele opdrachten zijn belangrijk ? :
+// debug de state door in Components weergave naar de stateful component te gaan
+
+class Stateful extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      styling: {},
+      styling2: {}
+    }
+    this.mouseOverHandler = this.mouseOverHandler.bind(this);
+    this.mouseOutHandler = this.mouseOutHandler.bind(this);
+  }
+
+  mouseOverHandler(e) {
+    e.target.className === 'buttonEen' ?
+    this.setState({styling: {
+      background: 'linear-gradient(210deg, #f00 0%, #e619e6 25%, #8000ff 50%, #0080ff 75%, #00eaff 100%)'
+    }}) 
+    :
+      this.setState({styling2: {
+        background: 'linear-gradient(210deg, #f00 0%, #e619e6 25%, #8000ff 50%, #0080ff 75%, #00eaff 100%)'
+    }})
+  }
+
+  mouseOutHandler(e) {
+    e.target.className === 'buttonEen' ?
+    this.setState({styling: {
+      color: 'linear-gradient(210deg, #00eaff 0%, #0080ff 25%, #8000ff 50%, #e619e6 75%, #f00 100%)'
+    }}) 
+    :
+    this.setState({styling2: {
+      background: 'linear-gradient(210deg, #00eaff 0%, #0080ff 25%, #8000ff 50%, #e619e6 75%, #f00 100%)'
+    }})
+  }
+
+  render(){
+    return <Introduction styling={this.state.styling} styling2={this.state.styling2} handler={this.mouseOverHandler} handlerOut={this.mouseOutHandler} />
+  }
+}
+
 ReactDOM.render(
   <React.StrictMode>
-    <Introduction />
+    <Stateful />
   </React.StrictMode>,
   document.getElementById('root')
 );
