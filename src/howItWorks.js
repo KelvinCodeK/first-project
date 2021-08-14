b7c51b97f3cf1f899e498096ca762599f65e23e5
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-08-14 15:30:48 +0200

    added component and logic

---
+++ src/howItWorks.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './howItWorks.css';
+
+export default class HowItWorks extends React.Component {
+    render() {
+        return (
+            <div className="howItWorks">
+                <h1>Hoe het werkt</h1>
+                <p>Door online zoekvolume en weerdata te combineren krijg je inzicht in:</p>
+                <ul>
+                    <li>Optimale verkoopmomenten door het jaar heen</li>
+                    <li>Of warme/koude dagen jouw online verkoop kunnen beïnvloeden</li>
+                    <li>De producten die het meest worden beïnvloed door het weer</li>
+                </ul>
+                <button className="buttonTwee" onClick={this.props.onClickHowToGo}>aan de slag!</button>
+            </div>
+        )
+    }
+}
--- src/index.js
+++ src/index.js
@@ -3,21 +3,25 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import './norm.css'
 import Introduction from './Introduction';
+import HowItWorks from './howItWorks';
 
 // houdt de standaard structuur aan. Als dingen gebeuren zet je pas je de state aan en en stuur je de nieuwe state door met props
 // conditionele opdrachten zijn belangrijk ? :
 // debug de state door in Components weergave naar de stateful component te gaan
 //verwijder alle onnodige uitleg uit de documenten in de app
+// state logica kan in elkaar gaan overlopen. Stappen doorlopen om de juiste logica te vinden.
 
 class Stateful extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       clickHow: false,
-      clickGo: false
+      clickGo: false,
+      clickHowToGo: false
     }
     this.onClickHow = this.onClickHow.bind(this);
     this.onClickGo = this.onClickGo.bind(this);
+    this.onClickHowToGo = this.onClickHowToGo.bind(this);
   }
 
   onClickHow() {
@@ -26,11 +30,17 @@ class Stateful extends React.Component {
   onClickGo() {
     this.setState({clickGo: true});
   }
+  onClickHowToGo() {
+    this.setState({clickHowToGo: true});
+  }
+
+  
 
   render(){
     return (
     <div>
-    {this.state.clickHow || this.state.clickGo === true ? null : <Introduction onClickHow={this.onClickHow} onClickGo={this.onClickGo}/>}
+    {this.state.clickHow || this.state.clickGo ? null : <Introduction onClickHow={this.onClickHow} onClickGo={this.onClickGo}/>}
+    {this.state.clickHow && this.state.clickHowToGo === false ? <HowItWorks onClickHowToGo={this.onClickHowToGo}/> : null}
     </div>)
   }
 }
