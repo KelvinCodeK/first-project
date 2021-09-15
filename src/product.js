f6400bfbd4bcb162eeae380865cf9dce02831c00
KelvinCodeK <kelvinkoeleman@hotmail.com>
2021-09-15 15:39:00 +0200

    refactoring

--- src/product.js
+++ src/product.js
@@ -9,19 +9,19 @@ export default class Product extends React.Component {
                     <h1>Weather To Sell</h1>
                     {/* p verzet naar header */}
                     <p>{this.props.Language === 'dutch' ? 'Kies een periode en vul het product óf de productcategorie in, waarover jij meer te weten wil komen.'
-                        : 'Please note that the search data is based on The Netherlands. You will get the best results by using Dutch search terms.'}</p>
+                        : 'Please note that the search data is based on The Netherlands. You will get the best results'}</p>
                 </header>
                 <main>
                     <section className="zoekVak">
                         
                         <div className="search">
                         <select onChange={this.props.jaarOfMaanden}>
-                            <option>{this.props.Language === 'dutch' ? 'Periode' : 'Date range'}</option>
+                            <option>{this.props.Language === 'dutch' ? 'Periode' : 'Date'}</option>
                             <option value="jaar">{this.props.Language === 'dutch' ? 'Afgelopen jaar' : 'Past year'}</option>
                             <option value="90">{this.props.Language === 'dutch' ? 'Afgelopen drie maanden' : 'Past three months'}</option>
                         </select>
                         <div className="inputVak">
-                        <input placeholder={this.props.Language === 'dutch' ? "zoek naar een product" : 'Search for a product'} onKeyUp={this.props.keyUpHandler} type="text"></input>  
+                        <input placeholder={this.props.Language === 'dutch' ? "zoek naar een product" : 'Search a product'} onKeyUp={this.props.keyUpHandler} type="text"></input>  
                         <button onClick={this.props.chartClick}></button>  
                         </div>
                         </div>
